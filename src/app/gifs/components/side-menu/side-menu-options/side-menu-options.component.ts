b09395eccf23df1ac94afc009cade935c4c539d7
JMHadga <jose.galdon12@gmail.com>
2025-02-28 16:11:10 +0100

    RouterLinks desde componentes hijos, environments y path alias

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { MenuOptions } from '../../../interfaces/menu-options.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLinkActive, RouterLink],
   templateUrl: './side-menu-options.component.html',
-  styleUrl: './side-menu-options.component.css'
+  styleUrl: './side-menu-options.component.css',
 })
 export class SideMenuOptionsComponent {
-
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  companyName: 'App',
+  companySecondName: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // Apikeys
+
+  // URLS
+};
