b13bdc77bdf03c5a7aba65907ad0bb3ad918d343
JMHadga <jose.galdon12@gmail.com>
2025-04-06 17:45:52 +0200

    Fin app gifs

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companySecondName: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'kocZErL412byU5D8s1PeN1WiCXSPxlPd',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
